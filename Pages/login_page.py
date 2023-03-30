6f2d53e3f2621afcf02eb683ebda91d89f609f12
azmiblidaoui <azmi.blidaoui19@gmail.com>
2023-03-30 12:33:52 +0100

    login_page

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -1,13 +1,25 @@
-from basePage import BasePage
+from BasePage import *
 class LoginPage(BasePage):
-    email_address_field =(By.name, "username")
-    password_field  = (By.name,"password")
-    login_button = (By.XPATH,"//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
-    worning_message = (By.XPATH,"//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div[1]/div[1]/p")
+    username_field =(By.NAME, '')
+    password_field  = (By.NAME,'')
+    login_button = (By.XPATH,'')
+    worning_message = (By.XPATH,'')
+    def __init__(self, driver):
+        super().__init__(driver) #or BasePage.__init__(self,driver)
+        
+    def set_username(self,username_field,username):
+        self.set_value(self,username_field,username)
     
-
-
-
+    def set_password(self, password):
+        self.set_value(self.password_field, password)
     
+    def click_login_btn(self):
+        self.click_ele(self.login_button)
+
+    def login(self,username,password):
+        self.set_username(username)
+        self.set_password(password)
+        self.click_login_btn()
+
 
     
